1454b93d9eafb5213f549666b4186ff842689694
GianniPio <giannipiopellegrino00@gmail.com>
2022-01-17 15:53:53 +0100

    completed

--- index.php
+++ index.php
@@ -33,3 +33,51 @@
      *      Fantozzi 2: il ritorno di fantozzi | ???
      *      Peter Pan: il ritorno all'isola che non c'e' | Robin Budd
      */ -->
+
+     <?php
+
+     class Film {
+        public $titolo;
+        public $sottotitolo;
+        public $regista;
+        
+        public function __construct($titolo) {
+            $this -> titolo = $titolo;
+        }
+
+        public function getFullTitle() {
+            if ($this -> sottotitolo != "") {
+                return $this -> titolo . ": ". $this -> sottotitolo;
+            }else {
+                return $this -> titolo . ": " ;
+            }
+        }
+
+        public function __toString(){
+            if($this -> regista != "") {
+                return $this -> getFullTitle() . " | " . $this -> regista;
+            }else{
+            return $this -> getFullTitle() . " | ???" ;
+            }
+        }
+    
+     }
+
+     $f1 = new Film("Matrix");
+     $f2 = new Film("Fantozzi 2");
+     $f3 = new Film("Peter Pan");
+
+     $f1 -> sottotitolo = "";
+     $f2 -> sottotitolo = "il ritorno di Fantozzi";
+     $f3 -> sottotitolo = "il ritorno all'isola che non c'è";
+     
+     $f1 -> regista = "";
+     $f2 -> regista = "";
+     $f3 -> regista = "Robin Budd";
+
+     echo "film1: " . $f1 -> __toString() . "<br>";
+     echo "film2: " . $f2 -> __toString() . "<br>";
+     echo "film3: " . $f3 -> __toString() . "<br>";
+
+
+     ?>
